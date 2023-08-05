5c8785241824bd2b2c3b6fdfb7e4fc00022ace1c
nikogenix <nikogenix@gmail.com>
2023-08-05 21:31:45 +0300

    Adding some placeholder content

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,55 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useMemo, useState } from "react";
 import "./App.css";
+import { Button, Box, Container, Typography, Link, SvgIcon, CardHeader, TextField } from "@mui/material";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 function App() {
-	const [count, setCount] = useState(0);
+	const [location, setLocation] = useState("");
+	const [date, setDate] = useState("");
+	const [user, setUser] = useState("");
+
+	const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+	const theme = useMemo(
+		() =>
+			createTheme({
+				palette: {
+					mode: prefersDarkMode ? "dark" : "light",
+				},
+			}),
+		[prefersDarkMode]
+	);
 
 	return (
-		<>
-			<div>
-				<a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank" rel="noreferrer">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-			</div>
-			<h1>Vite & React</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-				<p>
-					Edit <code>src/App.jsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-		</>
+		<ThemeProvider theme={theme}>
+			<Container>
+				<Box>
+					<Typography variant="h1">Weather App</Typography>
+					{!user && (
+						<Button variant="contained" color="primary">
+							Login
+						</Button>
+					)}
+					{user && (
+						<>
+							<Button variant="contained" color="primary">
+								Settings
+							</Button>
+							<Button variant="contained" color="primary">
+								Logout
+							</Button>
+						</>
+					)}
+				</Box>
+				<Box>
+					<TextField label="Location" value={location} onChange={(e) => setLocation(e.target.value)} />
+					<TextField label="Date" value={date} onChange={(e) => setDate(e.target.value)} />
+				</Box>
+				<Box>
+					<Typography variant="p">Weather Info</Typography>
+				</Box>
+			</Container>
+		</ThemeProvider>
 	);
 }
 
