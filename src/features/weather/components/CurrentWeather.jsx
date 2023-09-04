063db9710fb483e703d41b21e95631cfb71c1d5a
nikogenix <nikogenix@gmail.com>
2023-09-04 03:27:21 +0300

    Moving weather codes to data folder. Fixing data display bug

--- src/features/weather/components/CurrentWeather.jsx
+++ src/features/weather/components/CurrentWeather.jsx
@@ -7,148 +7,7 @@ import WeatherDetail from "./WeatherDetail";
 import "../../../assets/css/weather-icons.min.css";
 import "../../../assets/css/weather-icons-wind.min.css";
 
-const WEATHER_CODES = {
-	0: {
-		description: "clear sky",
-		dayIcon: "wi-day-sunny",
-		nightIcon: "wi-night-clear",
-	},
-	1: {
-		description: "mainly clear",
-		dayIcon: "wi-day-sunny-overcast",
-		nightIcon: "wi-night-alt-partly-cloudy",
-	},
-	2: {
-		description: "partly cloudy",
-		dayIcon: "wi-day-cloudy",
-		nightIcon: "wi-night-alt-cloudy",
-	},
-	3: {
-		description: "overcast",
-		dayIcon: "wi-cloudy",
-		nightIcon: "wi-cloudy",
-	},
-	45: {
-		description: "fog",
-		dayIcon: "wi-day-fog",
-		nightIcon: "wi-night-fog",
-	},
-	48: {
-		description: "depositing rime fog",
-		dayIcon: "wi-day-fog",
-		nightIcon: "wi-night-fog",
-	},
-	51: {
-		description: "drizzle: light intensity",
-		dayIcon: "wi-day-sprinkle",
-		nightIcon: "wi-night-alt-sprinkle",
-	},
-	53: {
-		description: "drizzle: moderate intensity",
-		dayIcon: "wi-day-sprinkle",
-		nightIcon: "wi-night-alt-sprinkle",
-	},
-	55: {
-		description: "drizzle: dense intensity",
-		dayIcon: "wi-day-sprinkle",
-		nightIcon: "wi-night-alt-sprinkle",
-	},
-	56: {
-		description: "freezing drizzle: light intensity",
-		dayIcon: "wi-day-sprinkle",
-		nightIcon: "wi-night-alt-sprinkle",
-	},
-	57: {
-		description: "freezing drizzle: dense intensity",
-		dayIcon: "wi-day-sprinkle",
-		nightIcon: "wi-night-alt-sprinkle",
-	},
-	61: {
-		description: "rain: slight intensity",
-		dayIcon: "wi-day-rain-mix",
-		nightIcon: "wi-night-alt-rain-mix",
-	},
-	63: {
-		description: "rain: moderate intensity",
-		dayIcon: "wi-day-rain",
-		nightIcon: "wi-night-alt-rain",
-	},
-	65: {
-		description: "rain: heavy intensity",
-		dayIcon: "wi-day-rain-wind",
-		nightIcon: "wi-night-alt-rain-wind",
-	},
-	66: {
-		description: "freezing rain: light intensity",
-		dayIcon: "wi-day-sleet",
-		nightIcon: "wi-night-alt-sleet",
-	},
-	67: {
-		description: "freezing rain: heavy intensity",
-		dayIcon: "wi-day-sleet",
-		nightIcon: "wi-night-alt-sleet",
-	},
-	71: {
-		description: "snow fall: slight intensity",
-		dayIcon: "wi-day-snow",
-		nightIcon: "wi-night-snow",
-	},
-	73: {
-		description: "snow fall: moderate intensity",
-		dayIcon: "wi-day-snow",
-		nightIcon: "wi-night-snow",
-	},
-	75: {
-		description: "snow fall: heavy intensity",
-		dayIcon: "wi-day-snow-wind",
-		nightIcon: "wi-night-snow-wind",
-	},
-	77: {
-		description: "snow grains",
-		dayIcon: "wi-day-snow",
-		nightIcon: "wi-night-snow",
-	},
-	80: {
-		description: "rain showers: slight intensity",
-		dayIcon: "wi-day-showers",
-		nightIcon: "wi-night-alt-showers",
-	},
-	81: {
-		description: "rain showers: moderate intensity",
-		dayIcon: "wi-day-showers",
-		nightIcon: "wi-night-alt-showers",
-	},
-	82: {
-		description: "rain showers: violent intensity",
-		dayIcon: "wi-day-showers",
-		nightIcon: "wi-night-alt-showers",
-	},
-	85: {
-		description: "snow showers: slight intensity",
-		dayIcon: "wi-day-snow",
-		nightIcon: "wi-night-alt-snow",
-	},
-	86: {
-		description: "snow showers: heavy intensity",
-		dayIcon: "wi-day-snow-wind",
-		nightIcon: "wi-night-alt-snow-wind",
-	},
-	95: {
-		description: "thunderstorm: slight or moderate",
-		dayIcon: "wi-day-thunderstorm",
-		nightIcon: "wi-night-alt-thunderstorm",
-	},
-	96: {
-		description: "thunderstorm with slight hail",
-		dayIcon: "wi-day-snow-thunderstorm",
-		nightIcon: "wi-night-snow-thunderstorm",
-	},
-	99: {
-		description: "thunderstorm with heavy hail",
-		dayIcon: "wi-day-snow-thunderstorm",
-		nightIcon: "wi-night-snow-thunderstorm",
-	},
-};
+import { WEATHER_CODES } from "../../../data/weather";
 
 const CurrentWeather = () => {
 	const { data } = useSelector((state) => state.weather);
@@ -161,6 +20,8 @@ const CurrentWeather = () => {
 		? `${new Date(date).getHours().toString().padStart(2, "0")}:00`
 		: data.current_weather.time.slice(11, 16);
 	const forecastStartIndex = Number(selectedTime.slice(0, 2));
+
+	console.log(forecastStartIndex);
 
 	return (
 		<Card sx={{ display: "flex", flexDirection: "column" }}>
@@ -177,10 +38,10 @@ const CurrentWeather = () => {
 					}`}
 					subheader={
 						/^[+-]\d+$/.test(data.timezone_abbreviation)
-							? `${dayjs(data.daily.time[forecastStartIndex]).format("MMMM D,")} ${selectedTime} UTC${
+							? `${dayjs(data.hourly.time[forecastStartIndex]).format("MMMM D,")} ${selectedTime} UTC${
 									data.timezone_abbreviation
 							  }`
-							: `${dayjs(data.daily.time[forecastStartIndex]).format("MMMM D,")} ${selectedTime} ${
+							: `${dayjs(data.hourly.time[forecastStartIndex]).format("MMMM D,")} ${selectedTime} ${
 									data.timezone_abbreviation
 							  }`
 					}
