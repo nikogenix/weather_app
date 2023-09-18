21fa8d1b1539a45f2297e02cb184884d18747c82
nikogenix <nikogenix@gmail.com>
2023-09-18 17:30:35 +0300

    Updating weather graphs

--- src/features/weather/components/WeatherGraphs.jsx
+++ src/features/weather/components/WeatherGraphs.jsx
@@ -1,5 +1,10 @@
 import * as React from "react";
+
 import dayjs from "dayjs";
+import timezone from "dayjs/plugin/timezone";
+import utc from "dayjs/plugin/utc";
+dayjs.extend(utc);
+dayjs.extend(timezone);
 
 import ToggleButton from "@mui/material/ToggleButton";
 import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
@@ -16,6 +21,8 @@ export default function WeatherGraphs({ data }) {
 		feelsLike: [],
 		precipitation: [],
 		wind: [],
+		uv: [],
+		aqi: [],
 	});
 
 	const handleChange = (event, newDay) => {
@@ -30,13 +37,16 @@ export default function WeatherGraphs({ data }) {
 	React.useEffect(() => {
 		if (data.hourly) {
 			const start = data.hourly.time.findIndex((c) => c.includes(daySelection));
-			const end = data.hourly.time.findLastIndex((c) => c.includes(daySelection));
+			const end = data.hourly.time.findLastIndex((c) => c.includes(daySelection)) + 1;
 
-			const hours = data.hourly.time.slice(start, end + 1).map((c) => c.slice(-5, -3));
+			const hours = data.hourly.time.slice(start, end + 1).map((c) => dayjs.tz(c, data.location.timezone));
 			const temperature = data.hourly.temperature_2m.slice(start, end + 1);
 			const feelsLike = data.hourly.apparent_temperature.slice(start, end + 1);
 			const precipitation = data.hourly.precipitation_probability.slice(start, end + 1);
 			const wind = data.hourly.windspeed_10m.slice(start, end + 1);
+			const windUnit = data.hourly_units.windspeed_10m;
+			const uv = data.hourly.uv_index.slice(start, end + 1);
+			const aqi = data.aqi.hourly.european_aqi.slice(start, end + 1);
 
 			setChartData({
 				hours,
@@ -44,6 +54,9 @@ export default function WeatherGraphs({ data }) {
 				feelsLike,
 				precipitation,
 				wind,
+				windUnit,
+				uv,
+				aqi,
 			});
 		}
 	}, [data, daySelection]);
@@ -56,6 +69,7 @@ export default function WeatherGraphs({ data }) {
 			label: "temperature",
 			showMark: false,
 			curve: "natural",
+			valueFormatter: (v) => v + "°",
 		},
 		{
 			yAxisKey: "linearAxis",
@@ -64,26 +78,61 @@ export default function WeatherGraphs({ data }) {
 			label: "feels like",
 			showMark: false,
 			curve: "natural",
+			valueFormatter: (v) => v + "°",
 		},
 		{
 			yAxisKey: "linearAxis",
 			data: chartData.precipitation,
 			color: "#4e79a7",
-			label: "precipitation",
+			label: "precipitation chance",
 			showMark: false,
 			curve: "natural",
+			area: true,
+			valueFormatter: (v) => v + "%",
 		},
 		{
 			yAxisKey: "linearAxis",
 			data: chartData.wind,
 			color: "#bab0ab",
-			label: "wind",
+			label: "wind speed",
+			showMark: false,
+			curve: "natural",
+			valueFormatter: (v) => `${v} ${chartData.windUnit}`,
+		},
+		{
+			yAxisKey: "linearAxis",
+			data: chartData.uv,
+			color: "#f28e2c",
+			label: "UV",
+			showMark: false,
+			curve: "natural",
+			area: true,
+		},
+		{
+			yAxisKey: "linearAxis",
+			data: chartData.aqi,
+			color: "#4e79a7",
+			label: "air quality",
 			showMark: false,
 			curve: "natural",
 		},
 	];
 
-	const filteredSeries = series.filter((c) => !c.data.includes(null) && c.data.length !== 0);
+	const filteredTempSeries = series.filter(
+		(c) => !c.data.includes(null) && c.data.length !== 0 && (c.label === "temperature" || c.label === "feels like")
+	);
+	const filteredPrecipitationSeries = series.filter(
+		(c) => !c.data.includes(null) && c.data.length !== 0 && c.label === "precipitation chance"
+	);
+	const filtereWindSeries = series.filter(
+		(c) => !c.data.includes(null) && c.data.length !== 0 && c.label === "wind speed"
+	);
+	const filteredUvSeries = series.filter((c) => !c.data.includes(null) && c.data.length !== 0 && c.label === "UV");
+	const filteredAqiSeries = series.filter(
+		(c) => !c.data.includes(null) && c.data.length !== 0 && c.label === "air quality"
+	);
+
+	const dateValueFormatter = (date) => dayjs.tz(date, data.location.timezone).format("h A");
 
 	return (
 		<>
@@ -121,17 +170,64 @@ export default function WeatherGraphs({ data }) {
 					))}
 				</ToggleButtonGroup>
 			</Paper>
-			<Paper>
-				{filteredSeries && (
-					<LineChart
-						xAxis={[{ data: chartData.hours }]}
-						yAxis={[{ id: "linearAxis", scaleType: "linear" }]}
-						series={filteredSeries}
-						leftAxis="linearAxis"
-						height={400}
-						sx={{ "--ChartsLegend-itemWidth": "120px" }}
-					/>
-				)}
+			<Paper sx={{ pt: 3 }}>
+				{[
+					filteredTempSeries,
+					filteredPrecipitationSeries,
+					filtereWindSeries,
+					filteredUvSeries,
+					filteredAqiSeries,
+				].map((c) => {
+					return (
+						c.length > 0 && (
+							<LineChart
+								key={c[0].label}
+								xAxis={[
+									{
+										data: chartData.hours,
+										scaleType: "time",
+										tickMinStep: 3600 * 1000 * 4,
+										valueFormatter: dateValueFormatter,
+										min: Number(chartData.hours[0]),
+										max: Number(chartData.hours[24]),
+									},
+								]}
+								yAxis={[
+									{
+										id: "linearAxis",
+										scaleType: "linear",
+										min: c[0].label === "air quality" ? 0 : undefined,
+									},
+								]}
+								series={c}
+								leftAxis="linearAxis"
+								height={180}
+								sx={{
+									"--ChartsLegend-itemMarkSize": "10px",
+									"--ChartsLegend-rootOffsetX": "7em",
+									"--ChartsLegend-rootOffsetY": "-1em",
+								}}
+								legend={{
+									direction: "column",
+									position: {
+										vertical: "top",
+										horizontal: "left",
+									},
+								}}
+								margin={{
+									top: 50,
+									bottom: 30,
+									left: 40,
+									right: 40,
+								}}
+								axisHighlight={{
+									x: "band",
+									y: "none",
+								}}
+							/>
+						)
+					);
+				})}
 			</Paper>
 		</>
 	);
