b065f3e62d18b5b27ca5f37cd97d4464ee72fdba
nikogenix <nikogenix@gmail.com>
2023-11-06 22:36:53 +0200

    Adding aria-label and tooltip. Fixing typo

--- src/components/WeatherRecommendationIcon.jsx
+++ src/components/WeatherRecommendationIcon.jsx
@@ -14,7 +14,7 @@ import { ReactComponent as Shorts } from "../assets/icons/Shorts.svg";
 import { ReactComponent as WinterBoots } from "../assets/icons/WinterBoots.svg";
 import { ReactComponent as FallCoat } from "../assets/icons/FallCoat.svg";
 import { ReactComponent as NoElectronics } from "../assets/icons/NoElectronics.svg";
-import { ReactComponent as Snickers } from "../assets/icons/Snickers.svg";
+import { ReactComponent as Sneackers } from "../assets/icons/Sneackers.svg";
 import { ReactComponent as WinterJacket } from "../assets/icons/WinterJacket.svg";
 import { ReactComponent as GlovesCapComforter } from "../assets/icons/GlovesCapComforter.svg";
 import { ReactComponent as OpenWindows } from "../assets/icons/OpenWindows.svg";
@@ -31,47 +31,133 @@ import { ReactComponent as PulloverX3 } from "../assets/icons/PulloverX3.svg";
 import { ReactComponent as Sunscreen } from "../assets/icons/Sunscreen.svg";
 
 const iconComponents = {
-	Boots,
-	LongSleeve,
-	Sandals,
-	UmbrellaRainCoat,
-	ClosedWindows,
-	LoosePants,
-	Shirt,
-	Water,
-	Coat,
-	Mask,
-	Shorts,
-	WinterBoots,
-	FallCoat,
-	NoElectronics,
-	Snickers,
-	WinterJacket,
-	GlovesCapComforter,
-	OpenWindows,
-	SunCover,
-	WinterTrousers,
-	Hoodie,
-	Pullover,
-	SunHatUmbrella,
-	Jeans,
-	PulloverX2,
-	Sunglasses,
-	JeansX2,
-	PulloverX3,
-	Sunscreen,
+	Boots: {
+		component: Boots,
+		description: "boots",
+	},
+	LongSleeve: {
+		component: LongSleeve,
+		description: "long-sleeve shirt",
+	},
+	Sandals: {
+		component: Sandals,
+		description: "sandals",
+	},
+	UmbrellaRainCoat: {
+		component: UmbrellaRainCoat,
+		description: "umbrella and raincoat",
+	},
+	ClosedWindows: {
+		component: ClosedWindows,
+		description: "closed windows",
+	},
+	LoosePants: {
+		component: LoosePants,
+		description: "loose-fitting pants",
+	},
+	Shirt: {
+		component: Shirt,
+		description: "shirt",
+	},
+	Water: {
+		component: Water,
+		description: "water bottle",
+	},
+	Coat: {
+		component: Coat,
+		description: "coat",
+	},
+	Mask: {
+		component: Mask,
+		description: "mask",
+	},
+	Shorts: {
+		component: Shorts,
+		description: "shorts",
+	},
+	WinterBoots: {
+		component: WinterBoots,
+		description: "winter boots",
+	},
+	FallCoat: {
+		component: FallCoat,
+		description: "fall coat",
+	},
+	NoElectronics: {
+		component: NoElectronics,
+		description: "no electronics - overheating risk",
+	},
+	Snickers: {
+		component: Sneackers,
+		description: "sneakers",
+	},
+	WinterJacket: {
+		component: WinterJacket,
+		description: "winter jacket",
+	},
+	GlovesCapComforter: {
+		component: GlovesCapComforter,
+		description: "gloves, cap, and comforter",
+	},
+	OpenWindows: {
+		component: OpenWindows,
+		description: "open windows",
+	},
+	SunCover: {
+		component: SunCover,
+		description: "cover-up for sun protection",
+	},
+	WinterTrousers: {
+		component: WinterTrousers,
+		description: "winter trousers",
+	},
+	Hoodie: {
+		component: Hoodie,
+		description: "hoodie",
+	},
+	Pullover: {
+		component: Pullover,
+		description: "pullover",
+	},
+	SunHatUmbrella: {
+		component: SunHatUmbrella,
+		description: "sun hat and umbrella",
+	},
+	Jeans: {
+		component: Jeans,
+		description: "jeans",
+	},
+	PulloverX2: {
+		component: PulloverX2,
+		description: "pullover (2 layers)",
+	},
+	Sunglasses: {
+		component: Sunglasses,
+		description: "sunglasses",
+	},
+	JeansX2: {
+		component: JeansX2,
+		description: "jeans (2 layers)",
+	},
+	PulloverX3: {
+		component: PulloverX3,
+		description: "pullover (3 layers)",
+	},
+	Sunscreen: {
+		component: Sunscreen,
+		description: "sunscreen",
+	},
 };
 
 const WeatherRecommendationIcon = ({ iconName, size, color }) => {
-	const IconComponent = iconComponents[iconName];
+	const IconData = iconComponents[iconName];
 
-	if (!IconComponent) return <>missing icon</>;
+	if (!IconData) return <>missing icon</>;
 
 	return (
-		<Tooltip title="">
-			{/* TODO: aria labels & tooltip */}
+		<Tooltip title={IconData.description}>
 			<SvgIcon viewBox="0 0 40 40" style={{ fontSize: size, color }}>
-				<IconComponent style={{ fontSize: size }} />
+				<IconData.component style={{ fontSize: size }} />
 			</SvgIcon>
 		</Tooltip>
 	);
