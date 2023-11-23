f441a1548cc1a121b36d7cfbfd64ada5390c218c
nikogenix <nikogenix@gmail.com>
2023-11-24 00:46:50 +0200

    Adding settings slice

--- src/store/settingsSlice.js
+++ src/store/settingsSlice.js
@@ -5,6 +5,61 @@ const settingsSlice = createSlice({
 	initialState: {
 		darkMode: true,
 		temperatureUnit: "C",
+		preferences: {
+			upperClothing: {
+				values: [-10, 0, 10, 20],
+			},
+			upperClothingLayer: {
+				values: [-10, 0, 10, 20],
+				jacketIfSnow: true,
+				coverIfSunAndUvThreshold: { enabled: true, value: 3 },
+			},
+			lowerClothing: {
+				values: [-10, 0, 10, 20],
+				rainNoShorts: true,
+				trousersIfSnow: true,
+			},
+			boots: {
+				values: [0, 10, 20],
+				rainNoSandals: true,
+				bootsIfSnow: true,
+			},
+			accessories: {
+				umbrella: {
+					rain: true,
+				},
+				sunglasses: {
+					ifUvThreshold: { enabled: true, value: 3 },
+				},
+				sunHat: {
+					ifUvThreshold: { enabled: true, value: 3 },
+				},
+				gloves: {
+					ifTempThreshold: { enabled: true, value: 0 },
+					ifSnow: true,
+				},
+			},
+			miscellaneous: {
+				spf: {
+					ifUvThreshold: { enabled: true, value: 3 },
+				},
+				mask: {
+					ifAqiThreshold: { enabled: true, value: 20 },
+				},
+				water: {
+					ifTempThreshold: { enabled: true, value: 20 },
+					always: true,
+				},
+				electronicsOverheat: {
+					ifTempThreshold: { enabled: true, value: 30 },
+				},
+				windows: {
+					openIfTempThreshold: { enabled: true, minValue: 15, maxValue: 25 },
+					openIfAqiThreshold: { enabled: true, value: 20 },
+					closeIfRainOrSnow: true,
+				},
+			},
+		},
 	},
 	reducers: {
 		toggleDarkMode: (state) => {
@@ -13,9 +68,12 @@ const settingsSlice = createSlice({
 		toggleTemperatureUnit: (state) => {
 			state.temperatureUnit = state.temperatureUnit === "C" ? "F" : "C";
 		},
+		updatePreferences: (state, action) => {
+			state.preferences = action.payload;
+		},
 	},
 });
 
-export const { toggleDarkMode, toggleTemperatureUnit } = settingsSlice.actions;
+export const { toggleDarkMode, toggleTemperatureUnit, updatePreferences } = settingsSlice.actions;
 
 export default settingsSlice.reducer;
