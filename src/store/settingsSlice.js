ab61e9f873d2cdd3aed60718d2933b044a3eeb1b
nikogenix <nikogenix@gmail.com>
2023-11-29 01:21:23 +0200

    Converting setting temps uppon unit toggling

--- src/store/settingsSlice.js
+++ src/store/settingsSlice.js
@@ -1,5 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+import { convertBetweenCandF } from "../utils/formatWeatherData";
+
 const settingsSlice = createSlice({
 	name: "settings",
 	initialState: {
@@ -66,7 +68,42 @@ const settingsSlice = createSlice({
 			state.darkMode = !state.darkMode;
 		},
 		toggleTemperatureUnit: (state) => {
+			const currentUnit = state.temperatureUnit;
+
 			state.temperatureUnit = state.temperatureUnit === "C" ? "F" : "C";
+
+			state.preferences.upperClothing.values = state.preferences.upperClothing.values.map((temp) =>
+				convertBetweenCandF(temp, currentUnit)
+			);
+			state.preferences.upperClothingLayer.values = state.preferences.upperClothingLayer.values.map((temp) =>
+				convertBetweenCandF(temp, currentUnit)
+			);
+			state.preferences.lowerClothing.values = state.preferences.lowerClothing.values.map((temp) =>
+				convertBetweenCandF(temp, currentUnit)
+			);
+			state.preferences.boots.values = state.preferences.boots.values.map((temp) =>
+				convertBetweenCandF(temp, currentUnit)
+			);
+			state.preferences.accessories.gloves.ifTempThreshold.value = convertBetweenCandF(
+				state.preferences.miscellaneous.water.ifTempThreshold.value,
+				currentUnit
+			);
+			state.preferences.miscellaneous.water.ifTempThreshold.value = convertBetweenCandF(
+				state.preferences.miscellaneous.water.ifTempThreshold.value,
+				currentUnit
+			);
+			state.preferences.miscellaneous.electronicsOverheat.ifTempThreshold.value = convertBetweenCandF(
+				state.preferences.miscellaneous.electronicsOverheat.ifTempThreshold.value,
+				currentUnit
+			);
+			state.preferences.miscellaneous.windows.openIfTempThreshold.minValue = convertBetweenCandF(
+				state.preferences.miscellaneous.windows.openIfTempThreshold.minValue,
+				currentUnit
+			);
+			state.preferences.miscellaneous.windows.openIfTempThreshold.maxValue = convertBetweenCandF(
+				state.preferences.miscellaneous.windows.openIfTempThreshold.maxValue,
+				currentUnit
+			);
 		},
 		updatePreferences: (state, action) => {
 			state.preferences = action.payload;
