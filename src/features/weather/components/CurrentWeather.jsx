7e68e26aa0e656e1cc62b09db8c16af7b13363dc
nikogenix <nikogenix@gmail.com>
2023-12-17 01:03:40 +0200

    Adding recommendations in the current weather component

--- src/features/weather/components/CurrentWeather.jsx
+++ src/features/weather/components/CurrentWeather.jsx
@@ -6,6 +6,7 @@ import CurrentWeatherDetail from "./CurrentWeatherDetail";
 import { formatWeatherData } from "../../../utils/formatWeatherData";
 import AirQualityTable from "./AirQualityTable";
 import UvIndexTable from "./UvIndexTable";
+import Recommendations from "../../recommendations";
 
 const CurrentWeather = ({ data, selectedTime, forecastStartIndex, weatherWarning }) => {
 	return (
@@ -45,140 +46,152 @@ const CurrentWeather = ({ data, selectedTime, forecastStartIndex, weatherWarning
 				)}
 			</Box>
 
-			{/* weather */}
-			<CardContent
-				sx={{
-					display: "flex",
-					alignItems: "center",
-					justifyContent: "space-around",
-					flexDirection: "column",
-					width: "100%",
-					maxWidth: 400,
-					alignSelf: "left",
-				}}
-			>
-				{/* icon and temperature */}
-				<Box
+			<Box sx={{ display: "flex", flexDirection: { xs: "column", md: "row" } }}>
+				{/* weather */}
+				<CardContent
 					sx={{
 						display: "flex",
 						alignItems: "center",
-						justifyItems: "center",
+						justifyContent: "space-around",
+						flexDirection: "column",
+						width: "100%",
+						maxWidth: 400,
+						alignSelf: "left",
 					}}
 				>
+					{/* icon and temperature */}
 					<Box
 						sx={{
 							display: "flex",
-							justifyItems: "center",
-							flexFlow: "column",
 							alignItems: "center",
+							justifyItems: "center",
 						}}
 					>
-						<Icon
-							component="i"
-							sx={{ fontSize: 80, overflow: "visible", width: "min-content" }}
-							baseClassName={`wi ${formatWeatherData(
-								data.hourly.weathercode[forecastStartIndex],
-								data.hourly.is_day[forecastStartIndex],
-								"weather icon"
-							)}`}
-							aria-hidden={false}
-							aria-label={`weather icon - ${formatWeatherData(
-								data.hourly.weathercode[forecastStartIndex],
-								undefined,
-								"weather description"
-							)}`}
-							role="img"
-						></Icon>
+						<Box
+							sx={{
+								display: "flex",
+								justifyItems: "center",
+								flexFlow: "column",
+								alignItems: "center",
+							}}
+						>
+							<Icon
+								component="i"
+								sx={{ fontSize: 80, overflow: "visible", width: "min-content" }}
+								baseClassName={`wi ${formatWeatherData(
+									data.hourly.weathercode[forecastStartIndex],
+									data.hourly.is_day[forecastStartIndex],
+									"weather icon"
+								)}`}
+								aria-hidden={false}
+								aria-label={`weather icon - ${formatWeatherData(
+									data.hourly.weathercode[forecastStartIndex],
+									undefined,
+									"weather description"
+								)}`}
+								role="img"
+							></Icon>
+						</Box>
+						<Box sx={{ m: 3 }}>
+							<Typography fontSize={40}>
+								{formatWeatherData(
+									data.hourly.temperature_2m[forecastStartIndex],
+									data.hourly_units.temperature_2m,
+									"temperature"
+								)}
+							</Typography>
+							<Typography>
+								{formatWeatherData(
+									data.hourly.weathercode[forecastStartIndex],
+									undefined,
+									"weather description"
+								)}
+							</Typography>
+							<Typography>
+								{formatWeatherData(
+									data.hourly.apparent_temperature[forecastStartIndex],
+									data.hourly_units.temperature_2m,
+									"apparent temperature"
+								)}
+							</Typography>
+						</Box>
 					</Box>
-					<Box sx={{ m: 3 }}>
-						<Typography fontSize={40}>
-							{formatWeatherData(
-								data.hourly.temperature_2m[forecastStartIndex],
-								data.hourly_units.temperature_2m,
-								"temperature"
-							)}
-						</Typography>
-						<Typography>
-							{formatWeatherData(
-								data.hourly.weathercode[forecastStartIndex],
-								undefined,
-								"weather description"
-							)}
-						</Typography>
-						<Typography>
-							{formatWeatherData(
-								data.hourly.apparent_temperature[forecastStartIndex],
-								data.hourly_units.temperature_2m,
-								"apparent temperature"
-							)}
-						</Typography>
-					</Box>
-				</Box>
 
-				{/* weather details */}
-				<Box
+					{/* weather details */}
+					<Box
+						sx={{
+							display: "flex",
+							justifyContent: "center",
+							width: { xs: "100%", sm: "100%" },
+							mt: { xs: 3, sm: 0 },
+						}}
+					>
+						<Box sx={{ mr: 1, width: "40%" }}>
+							<CurrentWeatherDetail
+								header="air quality"
+								value={data.aqi.hourly.european_aqi[forecastStartIndex]}
+								unit={data.aqi.hourly_units.european_aqi}
+								type={"aqi"}
+								tooltip={<AirQualityTable value={data.aqi.hourly.european_aqi[forecastStartIndex]} />}
+							/>
+							<CurrentWeatherDetail
+								header="wind"
+								value={data.hourly.windspeed_10m[forecastStartIndex]}
+								unit={data.daily_units.windspeed_10m_max}
+								secondaryValue={data.hourly.winddirection_10m[forecastStartIndex]}
+								type={"misc"}
+							/>
+							<CurrentWeatherDetail
+								header="humidity"
+								value={data.hourly.relativehumidity_2m[forecastStartIndex]}
+								type="percentage"
+							/>
+							<CurrentWeatherDetail
+								header="visibility"
+								value={data.hourly.visibility[forecastStartIndex]}
+								unit={data.hourly_units.visibility}
+								type="misc integer"
+							/>
+						</Box>
+
+						<Box sx={{ textAlign: "right", ml: 1, width: "40%" }}>
+							<CurrentWeatherDetail
+								header="UV index"
+								value={data.hourly.uv_index[forecastStartIndex]}
+								type={"unitless"}
+								tooltip={<UvIndexTable value={data.hourly.uv_index[forecastStartIndex]} />}
+							/>
+							<CurrentWeatherDetail
+								header="surface pressure"
+								value={data.hourly.surface_pressure[forecastStartIndex]}
+								unit={data.hourly_units.surface_pressure}
+								type="misc integer"
+							/>
+							<CurrentWeatherDetail
+								header="dew point"
+								value={data.hourly.dewpoint_2m[forecastStartIndex]}
+								unit={data.hourly_units.dewpoint_2m}
+								type="temperature"
+							/>
+							<CurrentWeatherDetail
+								header="precipitation chance"
+								value={data.hourly.precipitation_probability[forecastStartIndex]}
+								type={"percentage"}
+							/>
+						</Box>
+					</Box>
+				</CardContent>
+
+				{/* recommendations */}
+				<CardContent
 					sx={{
-						display: "flex",
-						justifyContent: "center",
-						width: { xs: "100%", sm: "100%" },
-						mt: { xs: 3, sm: 0 },
+						maxWidth: 400,
+						width: "100%",
 					}}
 				>
-					<Box sx={{ mr: 1, width: "40%" }}>
-						<CurrentWeatherDetail
-							header="air quality"
-							value={data.aqi.hourly.european_aqi[forecastStartIndex]}
-							unit={data.aqi.hourly_units.european_aqi}
-							type={"aqi"}
-							tooltip={<AirQualityTable value={data.aqi.hourly.european_aqi[forecastStartIndex]} />}
-						/>
-						<CurrentWeatherDetail
-							header="wind"
-							value={data.hourly.windspeed_10m[forecastStartIndex]}
-							unit={data.daily_units.windspeed_10m_max}
-							secondaryValue={data.hourly.winddirection_10m[forecastStartIndex]}
-							type={"misc"}
-						/>
-						<CurrentWeatherDetail
-							header="humidity"
-							value={data.hourly.relativehumidity_2m[forecastStartIndex]}
-							type="percentage"
-						/>
-						<CurrentWeatherDetail
-							header="visibility"
-							value={data.hourly.visibility[forecastStartIndex]}
-							unit={data.hourly_units.visibility}
-							type="misc integer"
-						/>
-					</Box>
-
-					<Box sx={{ textAlign: "right", ml: 1, width: "40%" }}>
-						<CurrentWeatherDetail
-							header="UV index"
-							value={data.hourly.uv_index[forecastStartIndex]}
-							type={"unitless"}
-							tooltip={<UvIndexTable value={data.hourly.uv_index[forecastStartIndex]} />}
-						/>
-						<CurrentWeatherDetail
-							header="surface pressure"
-							value={data.hourly.surface_pressure[forecastStartIndex]}
-							unit={data.hourly_units.surface_pressure}
-							type="misc integer"
-						/>
-						<CurrentWeatherDetail
-							header="dew point"
-							value={data.hourly.dewpoint_2m[forecastStartIndex]}
-							unit={data.hourly_units.dewpoint_2m}
-							type="temperature"
-						/>
-						<CurrentWeatherDetail
-							header="precipitation chance"
-							value={data.hourly.precipitation_probability[forecastStartIndex]}
-							type={"percentage"}
-						/>
-					</Box>
-				</Box>
-			</CardContent>
+					<Recommendations data={data} type={"full"} iconSize={50} />
+				</CardContent>
+			</Box>
 		</Card>
 	);
 };
