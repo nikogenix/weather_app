e1f3319bd64c457c0a0dc8d226852dd0c1e98366
nikogenix <nikogenix@gmail.com>
2023-12-22 22:17:37 +0200

    Refactoring to display icons through another component. Error handling

--- src/features/recommendations/components/RecommendationsFullView.jsx
+++ src/features/recommendations/components/RecommendationsFullView.jsx
@@ -1,12 +1,13 @@
 import { Box, ToggleButton, ToggleButtonGroup, Typography } from "@mui/material";
 import { useState } from "react";
+import BoxWithTooltip from "./BoxWithTooltip";
 
 const RecommendationsFullView = ({ recommendations, alignment }) => {
 	const [minMaxAvgClothing, setMinMaxAvgClothing] = useState("avg");
 	const handleClothingToggle = (event, newValue) => {
 		if (newValue !== null) setMinMaxAvgClothing(newValue);
 	};
-
+	console.log(recommendations);
 	return (
 		<Box>
 			<Box sx={{ display: "flex", marginBottom: 2, justifyContent: alignment }}>
@@ -74,21 +75,31 @@ const RecommendationsFullView = ({ recommendations, alignment }) => {
 				</ToggleButtonGroup>
 			</Box>
 			<Box sx={{ display: "flex", justifyContent: alignment }}>
-				<Box
+				<BoxWithTooltip
+					content={recommendations.clothing.upperClothingLayer?.[minMaxAvgClothing]}
+					tooltipEnabled={recommendations.clothing.upperClothingLayer?.incompleteData}
 					sx={{
 						position: "relative",
 						top: 15,
 						left: 5,
-						minWidth: recommendations.clothing.upperClothing[minMaxAvgClothing].props.size,
+						minWidth: recommendations.clothing.upperClothing
+							? recommendations.clothing.upperClothing[minMaxAvgClothing].props.size
+							: "auto",
 					}}
-				>
-					{recommendations.clothing.upperClothingLayer[minMaxAvgClothing]}
-				</Box>
-				<Box>{recommendations.clothing.upperClothing[minMaxAvgClothing]}</Box>
-				<Box>{recommendations.clothing.lowerClothing[minMaxAvgClothing]}</Box>
-				<Box sx={{ position: "relative", top: 15, left: -10 }}>
-					{recommendations.clothing.boots[minMaxAvgClothing]}
-				</Box>
+				/>
+				<BoxWithTooltip
+					content={recommendations.clothing.upperClothing?.[minMaxAvgClothing]}
+					tooltipEnabled={recommendations.clothing.upperClothing?.incompleteData}
+				/>
+				<BoxWithTooltip
+					content={recommendations.clothing.lowerClothing?.[minMaxAvgClothing]}
+					tooltipEnabled={recommendations.clothing.lowerClothing?.incompleteData}
+				/>
+				<BoxWithTooltip
+					content={recommendations.clothing.boots?.[minMaxAvgClothing]}
+					tooltipEnabled={recommendations.clothing.boots?.incompleteData}
+					sx={{ position: "relative", top: 15, left: -10 }}
+				/>
 			</Box>
 
 			<Box sx={{ display: "flex", justifyContent: alignment }}>
@@ -107,10 +118,26 @@ const RecommendationsFullView = ({ recommendations, alignment }) => {
 				</Typography>
 			</Box>
 			<Box sx={{ display: "flex", justifyContent: alignment }}>
-				<Box>{recommendations.accessories.glovesCapComforter.icon}</Box>
-				<Box>{recommendations.accessories.sunHatUmbrella.icon}</Box>
-				<Box>{recommendations.accessories.sunglasses.icon}</Box>
-				<Box>{recommendations.accessories.umbrellaRainCoat.icon}</Box>
+				<BoxWithTooltip
+					content={recommendations.accessories.glovesCapComforter.icon}
+					tooltipEnabled={recommendations.accessories.glovesCapComforter.incompleteData}
+					disabled={recommendations.accessories.glovesCapComforter.disabled}
+				/>
+				<BoxWithTooltip
+					content={recommendations.accessories.sunHatUmbrella.icon}
+					tooltipEnabled={recommendations.accessories.sunHatUmbrella.incompleteData}
+					disabled={recommendations.accessories.sunHatUmbrella.disabled}
+				/>
+				<BoxWithTooltip
+					content={recommendations.accessories.sunglasses.icon}
+					tooltipEnabled={recommendations.accessories.sunglasses.incompleteData}
+					disabled={recommendations.accessories.sunglasses.disabled}
+				/>
+				<BoxWithTooltip
+					content={recommendations.accessories.umbrellaRainCoat.icon}
+					tooltipEnabled={recommendations.accessories.umbrellaRainCoat.incompleteData}
+					disabled={recommendations.accessories.umbrellaRainCoat.disabled}
+				/>
 			</Box>
 			<Box sx={{ display: "flex", justifyContent: alignment }}>
 				<Typography
@@ -128,11 +155,31 @@ const RecommendationsFullView = ({ recommendations, alignment }) => {
 				</Typography>
 			</Box>
 			<Box sx={{ display: "flex", justifyContent: alignment }}>
-				<Box>{recommendations.miscellaneous.sunscreen.icon}</Box>
-				<Box>{recommendations.miscellaneous.mask.icon}</Box>
-				<Box>{recommendations.miscellaneous.water.icon}</Box>
-				<Box>{recommendations.miscellaneous.noElectronics.icon}</Box>
-				<Box>{recommendations.miscellaneous.windows.icon}</Box>
+				<BoxWithTooltip
+					content={recommendations.miscellaneous.sunscreen.icon}
+					tooltipEnabled={recommendations.miscellaneous.sunscreen.incompleteData}
+					disabled={recommendations.miscellaneous.sunscreen.disabled}
+				/>
+				<BoxWithTooltip
+					content={recommendations.miscellaneous.mask.icon}
+					tooltipEnabled={recommendations.miscellaneous.mask.incompleteData}
+					disabled={recommendations.miscellaneous.mask.disabled}
+				/>
+				<BoxWithTooltip
+					content={recommendations.miscellaneous.water.icon}
+					tooltipEnabled={recommendations.miscellaneous.water.incompleteData}
+					disabled={recommendations.miscellaneous.water.disabled}
+				/>
+				<BoxWithTooltip
+					content={recommendations.miscellaneous.noElectronics.icon}
+					tooltipEnabled={recommendations.miscellaneous.noElectronics.incompleteData}
+					disabled={recommendations.miscellaneous.noElectronics.disabled}
+				/>
+				<BoxWithTooltip
+					content={recommendations.miscellaneous.windows.icon}
+					tooltipEnabled={recommendations.miscellaneous.windows.incompleteData}
+					disabled={recommendations.miscellaneous.windows.disabled}
+				/>
 			</Box>
 		</Box>
 	);
