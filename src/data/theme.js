e9f16a9e370fad79a22c28b93f17cf38b3033ca5
nikogenix <nikogenix@gmail.com>
2024-01-19 15:36:24 +0200

    Fixing bug with scrollbar style not applying. Adjusting style

--- src/data/theme.js
+++ src/data/theme.js
@@ -32,31 +32,30 @@ export const getDesignTokens = (mode) => ({
 	components: {
 		MuiCssBaseline: {
 			styleOverrides: {
-				body: {
-					scrollbarColor: mode === "light" ? "#6b6b6b #f0f0f0" : "#6b6b6b #2b2b2b",
-					"&::-webkit-scrollbar, & *::-webkit-scrollbar": {
-						backgroundColor: mode === "light" ? "#f0f0f0" : "#2b2b2b",
-						width: "12px",
-						height: "12px",
-					},
-					"&::-webkit-scrollbar-thumb, & *::-webkit-scrollbar-thumb": {
-						borderRadius: 8,
-						backgroundColor: "#6b6b6b",
-						minHeight: 24,
-						border: mode === "light" ? "3px solid #f0f0f0" : "3px solid #2b2b2b",
-					},
-					"&::-webkit-scrollbar-thumb:focus, & *::-webkit-scrollbar-thumb:focus": {
-						backgroundColor: "#959595",
-					},
-					"&::-webkit-scrollbar-thumb:active, & *::-webkit-scrollbar-thumb:active": {
-						backgroundColor: "#959595",
-					},
-					"&::-webkit-scrollbar-thumb:hover, & *::-webkit-scrollbar-thumb:hover": {
-						backgroundColor: "#959595",
-					},
-					"&::-webkit-scrollbar-corner, & *::-webkit-scrollbar-corner": {
-						backgroundColor: mode === "light" ? "#f0f0f0" : "#2b2b2b",
-					},
+				scrollbarColor: mode === "light" ? "#6b6b6b #f0f0f0" : "#6b6b6b #2b2b2b",
+				"&::-webkit-scrollbar, & *::-webkit-scrollbar": {
+					backgroundColor: mode === "light" ? "#39214c" : "#2b2b2b",
+					width: "12px",
+					height: "12px",
+					margin: 20,
+				},
+				"&::-webkit-scrollbar-thumb, & *::-webkit-scrollbar-thumb": {
+					borderRadius: 8,
+					backgroundColor: "#959595",
+					minHeight: 24,
+					border: mode === "light" ? "3px solid #39214c" : "3px solid #2b2b2b",
+				},
+				"&::-webkit-scrollbar-thumb:focus, & *::-webkit-scrollbar-thumb:focus": {
+					backgroundColor: "#c1c1c1",
+				},
+				"&::-webkit-scrollbar-thumb:active, & *::-webkit-scrollbar-thumb:active": {
+					backgroundColor: "#c1c1c1",
+				},
+				"&::-webkit-scrollbar-thumb:hover, & *::-webkit-scrollbar-thumb:hover": {
+					backgroundColor: "#c1c1c1",
+				},
+				"&::-webkit-scrollbar-corner, & *::-webkit-scrollbar-corner": {
+					backgroundColor: mode === "light" ? "#39214c" : "#2b2b2b",
 				},
 			},
 		},
